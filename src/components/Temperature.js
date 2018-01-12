30a8d6d74aa1534e53fda69bcc0988ba66ceb546
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-01-12 13:33:16 +0100

    Charts for temperature

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Line} from 'react-chartjs';
 
 import {
     createFragmentContainer,
@@ -7,23 +8,52 @@ import {
 
 class Temperature extends Component {
     render() {
+        let data = this.props.temperatures.edges.map(({node}) => {
+            return {
+                x: new Date(node.timestamp),
+                y: node.value,
+            }
+        });
+        const chartData = {
+            labels: data.map(p => p.x),
+            datasets: [{
+                label: 'Temperature',
+                pointDotRadius: 0,
+                data: data.map(p => p.y),
+            }]
+        };
+
+        const chartOptions = {
+            showTooltips: false,
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    distribution: 'series',
+                    ticks: {
+                        source: 'labels'
+                    }
+                }],
+                yAxes: [{
+                    scaleLabel: true,
+                    labelString: 'Value'
+                }]
+            }
+        };
+
         return (
-            <div>
-                {this.props.temperatures.edges.map(({node}) => (
-                    <div key={node.__id}>{node.value} ({node.timestamp})</div>
-                ))}
-            </div>
+            <Line data={chartData} options={chartOptions} width="640" height="480"/>
         )
     }
 }
 
+
 export default createFragmentContainer(Temperature, graphql`
-  fragment Temperature_temperatures on TemperatureConnection {
-      edges {
-          node {
-              timestamp
-              value
-          }
-      }
-  }
+    fragment Temperature_temperatures on TemperatureConnection {
+        edges {
+            node {
+                timestamp
+                value
+            }
+        }
+    }
 `)
